fa732e22708c2f2575fc5384bc8c8332551f24cc
heback <heback@gmail.com>
2023-05-01 10:05:37 +0900

    123

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.title('Hel;lo World!')
+st.title('Hello World!')
+
+name = 'Jungu Lee'
+st.text(f'Your name is {name}')
+
+st.markdown('This is **markdown**')
+st.json(
+    {'key1':'value1',
+     'key2':'value2'}
+)
+
+import pandas as pd
+st.subheader('IRIS data')
+df = pd.read_csv('data/iris.csv')
+st.dataframe(df)
